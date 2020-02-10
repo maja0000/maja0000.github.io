ba484641500b4f0fc6ad6a66c2daf07ef831f415
Maja Gach <majaanna94@gmail.com>
2020-02-10 15:53:12 +0100

    added component front page image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Onboarding from "./Component/Onboarding.js";
+import FrontPagePicture from "./Component/FrontPagePicture";
 
 // import mainpic from "./pictures/fog.jpg";
 
 function App() {
   return (
     <div className="App">
+      <FrontPagePicture />
       <Onboarding />
     </div>
   );
---
+++ src/Component/FrontPagePicture.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+class FrontPagePicture extends React.Component {}
+
+export default FrontPagePicture;
