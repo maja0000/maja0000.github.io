af608d5a598d09ee1287295e4097dc32cc7d25b9
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-02-11 17:04:11 +0100

    add impressum

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Onboarding from "./Component/Onboarding.js";
 import StickyFooter from "./Component/StickyFooter.js";
+import Legal from "./Component/Legal";
 
 // import mainpic from "./pictures/fog.jpg";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Onboarding />
+      <Legal />
       <StickyFooter />
     </div>
   );
