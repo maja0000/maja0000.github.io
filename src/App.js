e07a44b044ee0ce40a8ac4cdf514f2b360294abe
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-02-11 18:04:24 +0100

    Delete onboarding and add SignInSide materail ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import Onboarding from "./Component/Onboarding.js";
 import StickyFooter from "./Component/StickyFooter.js";
 import Legal from "./Component/Legal";
+import SignInSide from "./Component/SignInSide.js";
 
 // import mainpic from "./pictures/fog.jpg";
 
 function App() {
   return (
     <div className="App">
-      <Onboarding />
+      <SignInSide />
       <Legal />
       <StickyFooter />
     </div>
