4f987d343e9ad7199f98ab04804672c467397d3e
FluffynatorT800 <58825226+FluffynatorT800@users.noreply.github.com>
2020-02-19 15:26:28 +0100

    SearchbarDummy
    Searchbar dummy can be conected as needed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./App.css";
+import SearchBar from "./Component/Searchbar/Searchbar";
 
-import mainpic from "./pictures/fog.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={mainpic} alt="x" />
-        <p></p>
+        <SearchBar/>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
