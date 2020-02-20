cd7e8b45c496dfd6e00107eca8e32dd28bbaa5c2
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-02-20 10:20:03 +0100

    Extract searchbar and put into WeatherCard. Change layout.

--- src/Component/Searchbar/LocationSearchBar.js
+++ src/Component/Searchbar/LocationSearchBar.js
@@ -15,9 +15,9 @@ const useStyles = makeStyles(theme => ({
   backColor: {
     backgroundColor: "#B9BABA"
   },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
+  //   menuButton: {
+  //     marginRight: theme.spacing(2)
+  //   },
   title: {
     flexGrow: 1,
     display: "none",
@@ -27,15 +27,17 @@ const useStyles = makeStyles(theme => ({
   },
   search: {
     position: "relative",
+    left: "166%",
+    top: "20px",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25)
     },
-    marginLeft: 0,
+    // marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
+      //   marginLeft: theme.spacing(1),
       width: "auto"
     }
   },
@@ -69,34 +71,19 @@ export default function SearchBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.backColor}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <div className={classes.title} noWrap>
-            <Logo />
-          </div>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Location…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
+      </div>
     </div>
   );
 }
--- src/Pages/Legal.js
+++ src/Pages/Legal.js
@@ -1,10 +1,6 @@
-import React, { PureComponent } from "react";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import Typography from "@material-ui/core/Typography";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-// import Link from "@material-ui/core/Link";
-import "../Component/Legal.css";
+import "../css/Legal.css";
 
 function Legal() {
   const useStyles = makeStyles(theme => ({
