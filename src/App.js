e728163e0c4cdf7cac942258e6f9f3a590d965a1
Leonard <leonard.flachs@gmail.com>
2020-02-24 14:14:24 +0100

    fixing conflicts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from "react";
 import Header from "./Component/Header";
 import StickyFooter from "./Component/StickyFooter.js";
@@ -43,7 +19,6 @@ function App() {
       </Switch>
       <StickyFooter />
     </Router>
->>>>>>> dev
   );
 }
 
