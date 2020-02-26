249a8584a5f6ce54d87bce46aff3adee981d9f7f
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-02-26 10:24:17 +0100

    Add IP address geolocation function in weather display and set state to this location

--- src/Component/WeatherDisplay.js
+++ src/Component/WeatherDisplay.js
@@ -16,38 +16,69 @@ class WeatherDisplay extends React.Component {
   }
   /// call the fetch function
   componentDidMount() {
-    this.getAPI();
+    ///  call function for geolocation
+    this.getCityNameFromIp();
+    this.getWeather(); /*gave different name to differenciate the api fetches better*/
   }
+
+  //   displayWeatherFromIp = position => {
+  //     console.log(position);
+  //     let lng = position.coords.longitude;
+  //     let lat = position.coords.latitude;
+  //     console.log(`longitude: ${lng} | latitude: ${lat}`);
+  //   };
+
   // change location based on user's input
   handleChange = event => {
     console.log("change input");
     this.setState({ citySearch: event.target.value });
   };
+
   // this function gets called when user presses enter in searchbar
   searchForNewLocation = event => {
     console.log("click search");
-    this.getAPI();
+    this.getWeather();
   };
 
-  // get API function
-  getAPI() {
+  // get City name from ip address
+  getCityNameFromIp() {
+    fetch("http://ip-api.com/json/")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(
+            alert("Sorry, we could not find your location.")
+          );
+        }
+      })
+      .then(result => {
+        this.setState({
+          citySearch: result.city
+        });
+      })
+      .then(result => this.getWeather());
+  }
+
+  // get API function for current weather
+  getWeather() {
+    /*gave different name*/
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${this.state.citySearch}&units=metric&APPID=886d3852a40cc28c819dfcb6e2ae6402`
     )
       .then(response => {
-        if (response.ok){
-          return response.json() 
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(alert("Sorry, we could not find your city."));
         }
-      else{
-        return Promise.reject(alert("Sorry, we could not find your city."))
-      }
-        })
-    .then(result => {
-      this.setState({
-        weatherDisplay: result,
-        loading: false
       })
-    })   
+      .then(result => {
+        this.setState({
+          weatherDisplay: result,
+          loading: false
+        });
+      });
   }
   render() {
     const { loading } = this.state;
